db0a161dd3d1335fd383b18af37ca157a14a5f21
Vishal07yadav2004 <vishal07yadav2004@gmail.com>
2026-03-27 15:54:51 +0530

    keys updated

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,12 +8,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCFOMvpiDyQi-fHYkM2VWL5mFcG-VNueCo",
-  authDomain: "aurelia-jewellery.firebaseapp.com",
-  projectId: "aurelia-jewellery",
-  storageBucket: "aurelia-jewellery.firebasestorage.app",
-  messagingSenderId: "495857245287",
-  appId: "1:495857245287:web:7d7152b5fff21958d888c2"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
