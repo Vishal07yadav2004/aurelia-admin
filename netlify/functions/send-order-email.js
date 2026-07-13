a572b5bb162887430714834e04b18cad02525390
Jatin Singh <js.jatinsingh019@gmail.com>
2026-07-13 17:22:33 +0530

    fixes function

--- netlify/functions/send-order-email.js
+++ netlify/functions/send-order-email.js
@@ -6,11 +6,19 @@ const admin = require('firebase-admin');
 
 function initAdmin() {
   if (admin.apps.length) return;
+
+  console.log("FIREBASE_PROJECT_ID:", process.env.FIREBASE_PROJECT_ID);
+  console.log("FIREBASE_CLIENT_EMAIL:", process.env.FIREBASE_CLIENT_EMAIL);
+  console.log(
+    "Has FIREBASE_PRIVATE_KEY:",
+    !!process.env.FIREBASE_PRIVATE_KEY
+  );
+
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
     }),
   });
 }
